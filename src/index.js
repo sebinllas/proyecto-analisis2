a2cc4c2d5505c8bf5cdbea89ff817fcb160c7c08
Sebastian <sebastian.suarezr@udea.edu.co>
2021-05-15 23:23:53 -0500

    autenticación simple y dashboard que lista los usuarios  implementados

--- src/index.js
+++ src/index.js
@@ -48,17 +48,44 @@ handleLogin = (req, res) => {
   res.render("login.html");
 };
 handleLoginCss = (req, res) => {
-  res.sendFile(path.join(__dirname, 'styles', 'login.css'));
+  res.sendFile(path.join(__dirname, "styles", "login.css"));
+};
+handleUsersDash = (req, res) => {
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM users", (err, users) => {
+      if (err) {
+        res.json(err);
+      }
+      console.log(users);
+      res.render("users", {
+        data: users,
+      });
+    });
+  });
 };
 handleAuth = (req, res) => {
   const data = req.body;
-  console.log(data);
-  res.send(`data: ${JSON.stringify(data)}`);
-  
+  //console.log(data);
+  req.getConnection((err, conn) => {
+    const query = conn.query(
+      "SELECT * FROM users WHERE  userName = ? AND password = ?",
+      [data.userName, data.password],
+      (err, user) => {
+        console.log('user = ', user);
+        console.log(typeof user)
+        if(user[0] == undefined){
+          res.redirect('login')
+        }else{
+          res.send(`funcionó! jejejeje ${JSON.stringify(user[0])}`);
+        }
+        
+      }
+    );
+  });
 };
 
 //routing
-//const router = require('express').Router();
 app.get("/login", handleLogin);
 app.get("/src/styles/login.css", handleLoginCss);
 app.post("/auth", handleAuth);
+app.get("/admin/usersdashboard", handleUsersDash);
