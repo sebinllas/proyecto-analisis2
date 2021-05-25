a1d626072b5bb6f471be95cd0a30478dd231f350
Sebastian <sebastian.suarezr@udea.edu.co>
2021-05-24 22:48:13 -0500

    agregando la posibilidad de hacer posts, alerta de login incorrecto y más

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const util = require('util');
 
 handleLogin = (req, res) => {
-	res.render('login.html');
+	res.render('login');
 };
 handleUsersDash = (req, res) => {
 	if (!req.session.loggedin) {
@@ -31,7 +31,15 @@ handleAuth = (req, res) => {
 			[data.userName, data.password],
 			(err, user) => {
 				if (user[0] == undefined) {
-					res.redirect('login');
+					res.render('login', {
+						alert: true,
+						alertTitle: 'Error',
+						alertMessage: 'USUARIO y/o PASSWORD incorrectas',
+						alertIcon: 'error',
+						showConfirmButton: true,
+						timer: false,
+						ruta: 'login',
+					});
 				} else {
 					req.session.loggedin = true;
 					req.session.user = {
@@ -48,28 +56,32 @@ handleAuth = (req, res) => {
 		);
 	});
 };
-handlePosts = (req, res) => {
+handleClass = (req, res) => {
 	if (!req.session.loggedin) {
-		res.redirect('login');
-		return;
+		res.redirect('/login');
 	}
 	req.getConnection((err, conn) => {
-		conn.query('SELECT * FROM posts', (err, posts) => {
-			// posts.forEach((post) => {
-			// 	conn.query('SELECT fullName FROM users WHERE id = ?', [post.owner], (err, user) => {
-			// 		//console.log(user[0].fullName);
-			// 		post['ownerName'] = user[0].fullName;
-			// 	});
-
-			// });
-			for (let i = 0; i < posts.length; i++) {
-				conn.query('SELECT fullName FROM users WHERE id = ?', [posts[i].owner], (err, user) => {
-					posts[i]['ownerName'] = user[0].fullName;
-					if (i == posts.length - 1) {
-						res.render('posts', { posts });
-						console.log('post = ', JSON.stringify(posts));
-					}
-				});
+		conn.query(
+			'SELECT * FROM users_classes WHERE user = ? AND class = ?',
+			[req.session.user.id, req.params.class_id],
+			(err, user_class) => {
+				if (user_class.length < 1) {
+					res.redirect('/');
+				}
+			}
+		);
+		conn.query('SELECT * FROM posts WHERE class = ?', [req.params.class_id], (err, posts) => {
+			if (posts.length >= 1) {
+				for (let i = 0; i < posts.length; i++) {
+					conn.query('SELECT fullName FROM users WHERE id = ?', [posts[i].owner], (err, user) => {
+						posts[i]['ownerName'] = user[0].fullName;
+						if (i == posts.length - 1) {
+							res.render('posts', { posts });
+						}
+					});
+				}
+			} else {
+				res.render('posts', { posts: [{ content: '', class: req.params.class_id }] });
 			}
 
 			//res.send('estos son los posts\n' + JSON.stringify(posts));
@@ -85,8 +97,11 @@ handleMyCourses = (req, res) => {
 	req.getConnection((err, conn) => {
 		conn.query('SELECT class FROM users_classes WHERE user = ?', [req.session.user.id], (err, classes) => {
 			// conn.query('SELECT * FROM classes WHERE id = ')
-			console.log(classes);
+			//console.log('classes = ',classes, classes.length);
 			console.log(req.session);
+			if (classes.length == 0) {
+				res.render('my-courses', { allClasses: [] });
+			}
 			var allClasses = [];
 			for (let i = 0; i < classes.length; i++) {
 				console.log(classes[i].class);
@@ -103,11 +118,28 @@ handleMyCourses = (req, res) => {
 	});
 };
 
+handlePost = (req, res) => {
+	console.log(req.body);
+	console.log(req.params);
+	req.getConnection((err, conn) => {
+		conn.query(
+			'INSERT INTO `posts`(`id`, `content`, `owner`, `class`) VALUES (?, ?, ?, ?)',
+			['NULL', req.body.message, req.session.user.id, parseInt(req.params.class)],
+			(err, post) => {
+				console.log(err);
+				console.log(post);
+				res.redirect(`/class/${req.params.class}`);
+			}
+		);
+	});
+};
+
 //routing
 router.get('/login', handleLogin);
 router.post('/auth', handleAuth);
 router.get('/admin/usersdashboard', handleUsersDash);
-router.get('/posts', handlePosts);
+router.get('/class/:class_id', handleClass);
 router.get('/', handleMyCourses);
+router.post('/post/:class', handlePost);
 
 module.exports = router;
